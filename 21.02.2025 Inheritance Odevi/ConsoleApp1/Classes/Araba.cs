2d247619745b044f51c5302473d35bc62e5a955a
emirhn-aydnlk <emirhn.aydnlk@outlook.com>
2025-03-02 23:39:35 +0300

    21.02.2025 Inheritance Odevi
    Portal'a yüklenen ödevdeki hatalar düzeltildi.

---
+++ 21.02.2025 Inheritance Odevi/ConsoleApp1/Classes/Araba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Classes
+{
+    public class Araba
+    {
+        public string vites;
+        public string marka;
+        public void yaz(string vites, string marka)
+        {
+            Console.WriteLine(marka + " " + vites + " viteslidir");
+        }
+    }
+}
---
+++ 21.02.2025 Inheritance Odevi/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1.Classes;
+
+Bmw a = new Bmw();
+a.vites = "düz";
+a.marka = "BMW";
+a.yaz(a.vites,a.marka);
+
+Mercedes b = new Mercedes();
+b.vites = "otomatik";
+b.marka = "Mercedes";
+b.yaz(b.vites, b.marka);
+
+Toyota c = new Toyota();
+c.vites = "otomatik";
+c.marka = "Toyota";
+c.yaz(c.vites, c.marka);
