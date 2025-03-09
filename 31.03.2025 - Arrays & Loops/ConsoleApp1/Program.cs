addf3e344835ed74727a47eb33349378a0f576f5
emirhn-aydnlk <emirhn.aydnlk@outlook.com>
2025-03-09 17:36:29 +0300

    31.03.2025 Arrays & Loops

---
+++ 31.03.2025 - Arrays & Loops/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = new int[5];
+int toplam = 0;
+
+for (int i=0; i<array.Length; i++)
+{
+    Console.WriteLine(i + 1 + ". sayıyı girin: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    toplam += array[i];
+}
+Console.WriteLine("sayilarin ortalamasi : " + toplam / 5);
