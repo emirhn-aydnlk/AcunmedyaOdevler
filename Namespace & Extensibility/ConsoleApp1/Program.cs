46210526c50f6e80cfb24b0b04a4c18f7b89f59b
emirhn-aydnlk <emirhn.aydnlk@outlook.com>
2025-03-15 15:39:26 +0300

    Namespace & Extensibility

---
+++ Namespace & Extensibility/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using MatematikIslemleri;
+using System;
+
+namespace MatematikIslemleri
+{
+    public class Hesaplama
+    {
+        public void Topla(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+
+        public void Carp(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 * sayi2);
+        }
+    } 
+}
+
+namespace ProgramTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hesaplama hesaplama = new Hesaplama();
+
+            hesaplama.Topla(5, 5);
+            hesaplama.Carp(5, 5);  
+        }
+    }
+}
