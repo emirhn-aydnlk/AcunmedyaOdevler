714a5c01c999d9ed078287bbd5e5e534300b433e
emirhn-aydnlk <emirhn.aydnlk@outlook.com>
2025-04-07 19:57:01 +0300

    Generic List Odevi

---
+++ Generic List Odevi/GenericList/Araba.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class Araba
+    {
+        public string marka { get; set; } //Arabanın markası
+        public string model { get; set; } //Arabanın modeli
+        public double benzinHarcamasi { get; set; } //100km başına tükettiği yakıt miktarı
+        public double toplamMesafe { get; set; } //Arabanın toplamda kaç km yol gittiğini tutar
+
+        public double ToplamYakitTuketimi()
+        {
+            return (toplamMesafe / 100) * benzinHarcamasi;
+        }
+    }
+}
---
+++ Generic List Odevi/GenericList/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericList;
+
+    List<Araba> cars = new List<Araba>
+        {
+            new Araba { marka = "Toyota", model = "Corolla", benzinHarcamasi = 6.5, toplamMesafe = 6000 },
+            new Araba { marka = "BMW", model = "X5", benzinHarcamasi = 9.0, toplamMesafe = 4250 },
+            new Araba { marka = "Mercedes", model = "C200", benzinHarcamasi = 8.2, toplamMesafe = 5100 }
+        };
+
+    foreach (Araba car in cars)
+    {
+        double toplamYakit = car.ToplamYakitTuketimi();
+        Console.WriteLine($"Marka: {car.marka}, Model: {car.model}, 100 km'de Yaktığı Yakıt: {car.benzinHarcamasi} litre, Toplam Yakıt Tüketimi: {toplamYakit} litre");
+    }
